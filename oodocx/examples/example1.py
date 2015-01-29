1a1c6bf328c8ede4478561ebc8aef26c7854dd84
Cristian Anca <cristian.anca@gmail.com>
2015-01-29 17:32:07 +0200

    added example for table
    added a small example for testing the table align.

--- oodocx/examples/example1.py
+++ oodocx/examples/example1.py
@@ -4,3 +4,22 @@ d = oodocx.Docx()
 body = d.get_body()
 body.append(oodocx.paragraph('Hello World!'))
 d.save('Example1.docx')
+
+d = oodocx.Docx()
+body = d.get_body()
+body.append(oodocx.paragraph('Hello World!'))
+
+table_info = []
+table_info.append(["test1", "test2"])
+table_info.append(["test3", "test4"])
+table_info.append(["test5", "test6"])
+table_info.append(["test7", "test8"])
+
+body.append(oodocx.table(contents=table_info,
+                         heading=False,
+                         colw=[3348, 6228],
+                         borders={"all": {"color": "#000000", "sz": "4",
+                                  "val": "single"}},
+                         celstyle=[{"align": u"left"}, {"align": u"center"}]))
+
+d.save('ExampleTable.docx')
